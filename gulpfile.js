8b874909c8bf3e18dc6f1c1cfc6e8d983b7fcbbc
Rinat Sadykov <sadykovra@mail.ru>
2021-09-14 07:47:59 +0500

    add automatization

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const htmlmin = require('gulp-htmlmin');
+const jsmin = require('gulp-jsmin');
+const del = require('del');
 const sync = require("browser-sync").create();
 
 // Styles
-
 const styles = () => {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -17,11 +19,50 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Html
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"));
+}
+
+exports.html = html;
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/*.js")
+    .pipe(jsmin())
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+
+// Copy
+
+const copy = (done) => {
+  return gulp.src([
+    "source/fonts/*.{woff,woff2}",
+    "source/favicon/*.ico",
+    "source/img/**/*.{jpg,png,svg,webp}"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+  done();
+}
+
+exports.copy = copy;
 
 // Server
 
@@ -46,6 +87,39 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+// Delete build before
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
+
+
+
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    html,
+    styles,
+    scripts,
+    copy
+  )
 );
+
+exports.build = build;
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    copy
+  ),
+  gulp.series(
+    server,
+    watcher,
+    styles
+  )
+);
+
